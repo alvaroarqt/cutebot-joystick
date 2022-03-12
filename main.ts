165c8060587ddfeeb32b638341197b9eb456e7a3
Alvaro Moreno <98691922+alvaroarqt@users.noreply.github.com>
2022-03-12 12:05:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,8 @@ let x = 0
 radio.setGroup(1)
 basic.showIcon(IconNames.Sword)
 basic.forever(function () {
-    x = Math.map(pins.analogReadPin(AnalogPin.P0), 1023, 0, -100, 100)
-    y = Math.map(pins.analogReadPin(AnalogPin.P1), 1023, 0, -100, 100)
+    x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, 100, -100)
+    y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, -100, 100)
     radio.sendValue("x", x)
     basic.pause(100)
     radio.sendValue("y", y)
